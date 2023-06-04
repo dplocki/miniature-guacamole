50e95ea33aa54ef1058a0fc27c5b3befa93a68b3
Ziomioslaw <dawid.plocki@gmail.com>
2023-06-04 12:42:28 +0200

    Simplify the parser usage

--- features/test_calculator.py
+++ features/test_calculator.py
@@ -1,5 +1,6 @@
 from calculator import Calculator
-from pytest_bdd import given, scenarios, then, when, parsers
+from pytest_bdd import given, scenarios, then, when
+from pytest_bdd.parsers import parse
 
 scenarios("test_calculator.feature")
 
@@ -9,11 +10,11 @@ def given_calculator():
     return {"calculator": Calculator()}
 
 
-@when(parsers.parse("add number {a:d} to number {b:d}"))
+@when(parse("add number {a:d} to number {b:d}"))
 def when_add_number(context, a, b):
     context["result"] = context["calculator"].add(a, b)
 
 
-@then(parsers.parse("result should be equal {result:d}"))
+@then(parse("result should be equal {result:d}"))
 def then_result_should_be_equal(context, result):
     assert context["result"] == result
